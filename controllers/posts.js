bee701e125ca1b80637e60d7e38d69593ff91747
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2020-06-24 10:05:25 -0700

    Users can see individual posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,6 +2,9 @@ const Post = require('../models/post');
 
 module.exports = (app) => {
 
+ 
+
+
  // INDEX
  app.get('/', (req, res) => {
   Post.find({}).lean()
@@ -15,16 +18,34 @@ module.exports = (app) => {
    });
  })
 
- // CREATE
- app.post('/posts/new', (req, res) => {
-  // INSTANTIATE INSTANCE OF POST MODEL
-  const post = new Post(req.body);
+ // NEW
+ app.get('/posts/new/', (req, res) => {
+   res.render('posts-new', {});
+ })
+  // CREATE
+  app.post('/posts/new/', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    const post = new Post(req.body);
 
-  // SAVE INSTANCE OF POST MODEL TO DB
-  post.save((err, post) => {
-   // REDIRECT TO THE ROOT
-   return res.redirect(`/`);
+    // SAVE INSTANCE OF POST MODEL TO DB
+    post.save((err, post) => {
+      // REDIRECT TO THE ROOT
+      return res.redirect(`/`);
+    })
+  });
+ //SEE EACH POST
+app.get("/posts/:id", function (req, res) {
+ // LOOK UP THE POST
+ Post.findById(req.params.id)
+  .then(post => {
+   res.render("posts-show", {
+    post
+   });
   })
- });
+  .catch(err => {
+   console.log(err.message);
+  });
+});
+
 
 };
--- server.js
+++ server.js
@@ -1,56 +1,35 @@
-var exphbs = require('express-handlebars');
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
+const handlebars = require('handlebars');
+const {
+  allowInsecurePrototypeAccess
+} = require('@handlebars/allow-prototype-access');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  handlebars: allowInsecurePrototypeAccess(handlebars),
+});
+app.set('view engine', 'handlebars');
+
 
 // Use Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-
 require('./controllers/posts.js')(app);
+
 // Add after body parser initialization!
 app.use(expressValidator());
 
-app.engine('handlebars', exphbs({
- defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
 
 
 // Set db
 require('./data/reddit-db');
 
-var posts = [{
-  title: "I am your first event",
-  summary: "A great event that is super fun to look at and good",
-  url: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn"
- },
- {
-  title: "I am your second event",
-  summary: "A great event that is super fun to look at and good",
-  url: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn"
- },
- {
-  title: "I am your third event",
-  summary: "A great event that is super fun to look at and good",
-  url: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn"
- }
-]
-
-// INDEX
-app.get('/', (req, res) => {
- res.render('posts-index', {
-  posts: posts
- });
-})
-
-// NEW
-app.get('/posts/new', (req, res) => {
- res.render('posts-new', {});
-})
 
 
 app.listen(3000, () => {
